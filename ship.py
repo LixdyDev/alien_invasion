8bd387eecefa18565103a91e8ea083c70c15cdbc
LixdyDev <anurarohana1239@gmail.com>
2026-07-17 00:57:53 +0530

    feat: finalize fleet mechanics, implement entity collisions and game over states

--- ship.py
+++ ship.py
@@ -41,6 +41,11 @@ class Ship:
         # Update rect object from self.x.
         self.rect.x = self.x
         self.rect.y = self.y
+    
+    def center_ship(self):
+        """Center the ship on the screen."""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x - float(self.rect.x)
         
     def blitme(self):
         """Draw the ship at its current location."""
